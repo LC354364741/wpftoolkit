a88c50c20c571026f589f2025485b4381ec163d4
chao li <lichao354364741@gmail.com>
2025-05-07 19:58:16 +0800

    commit

---
+++ ExtendedWPFToolkitSolution/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Pg.SelectedObject = new Person();
+        }
+    }
+    public class Person
+    {
+        [Category("Prop")]
+        public int Age { get; set; }
+        [Category("Prop")]
+        public int Tel { get; set; }
+        [Category("Prop")]
+        public int Class { get; set; }
+    }
+}
